32dca6406266a1b82b03b3239ca757d69677d512
Niko Partanen <nikotapiopartanen@gmail.com>
2018-11-16 11:19:00 +0200

    removing the old models

--- scripts/2_train_models.sh
+++ scripts/2_train_models.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm ./train/mixed/*
+rm ./train/sjd/*
 
-/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain -F 100 -c ./train/mixed/*gt.txt --ntrain 100000 -o models/mixed/una- ./train/mixed/*bin.png
+/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain -F 100 -c ./train/mixed/*gt.txt --ntrain 100000 -o models/mixed/mixed ./train/mixed/*bin.png
+/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain -F 100 -c ./train/sjd/*gt.txt --ntrain 100000 -o models/sjd/sjd ./train/sjd/*bin.png
 
