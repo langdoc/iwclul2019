266837cf37811f6d5d229ae57b45ea25c14b35f3
Niko Partanen <nikotapiopartanen@gmail.com>
2018-12-20 09:04:50 +0200

    referencing to right version

--- scripts/0_prepare_data.sh
+++ scripts/0_prepare_data.sh
@@ -1,2 +1,3 @@
+mkdir data
 cd data
-git clone -b 'v0.9' --single-branch --depth 1 https://github.com/langdoc/unified-northern-alphabet-ocr.git
+git clone -b 'v1.1' --single-branch --depth 1 https://github.com/langdoc/unified-northern-alphabet-ocr.git
