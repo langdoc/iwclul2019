3db4216bff45b6749bc548c4a979f640127d8c87
Niko Partanen <nikotapiopartanen@gmail.com>
2018-12-20 17:04:19 +0200

    checking the evaluation script

--- scripts/4_evaluate_page_increase.sh
+++ scripts/4_evaluate_page_increase.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for model in `ls models/incr/*gz`
+mkdir evaluation
+
+for model in `ls models/incr/*10000.pyrnn.gz`
 do
   model_score=`echo ${model##*/}`
   /Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rpred -q -Q 4 -m $model 'test/sjd/*.bin.png'
