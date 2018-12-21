e6e2a489263f4a6d5e63087887c6e869c07cd74e
Niko Partanen <nikotapiopartanen@gmail.com>
2018-12-21 16:04:13 +0200

    final script

--- scripts/4_plot_page_increase.R
+++ scripts/4_plot_page_increase.R
@@ -17,6 +17,7 @@ score_incr_plot <- ggplot(data = errors, aes(x = model, y = error_rate, group = 
   geom_point() +
   geom_point(data = best_score, colour="red", size = 2) +  # this adds a red point
   geom_text(data = best_score, label = "", hjust=-.1) +
+  scale_y_continuous(breaks=c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)) +
   # scale_y_continuous(labels = scales::percent) +
   labs(x = "Number of training pages",
        y = "Error rate (%)")
@@ -25,4 +26,4 @@ dir.create("plots")
 
 ggsave(filename = "plots/figure_5.png", plot = score_incr_plot)
 
-"Test scores for Kildin Saami OCR model. Best score with error rate of 1.95%  marked with red"
+str_glue("Test scores for Kildin Saami OCR model. Best score with error rate of {best_score$error_rate}%  marked with red")
