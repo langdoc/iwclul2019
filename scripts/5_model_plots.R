c14c2449ba506ea75f1d9485182c3d22b120a3bd
Niko Partanen <nikotapiopartanen@gmail.com>
2018-12-22 14:05:53 +0200

    plotting script

--- scripts/5_model_plots.R
+++ scripts/5_model_plots.R
@@ -2,44 +2,6 @@ library(tidyverse)
 library(ggplot2)
 library(tidytext)
 
-# scores <- dir("evaluation/", full.names = TRUE, recursive = TRUE) %>%
-#   map_df(~ {
-#     read_tsv(.x, 
-#          col_names = c("errors", "characters", "line")) %>%
-#   mutate(lang = str_extract(line, "(mns|sel|sjd|yrk)")) %>%
-#   mutate(epoch = str_extract(.x, "\\d{8}") %>% as.numeric()) %>%
-#   mutate(score = errors / characters) %>%
-#   mutate(experiment = str_extract(.x, "(mixed|sjd)"))
-#   }
-# )
-# 
-# scores %>% 
-#   filter(experiment == "mixed") %>%
-#   group_by(epoch, lang) %>%
-#   summarise(error_mean = mean(score)) %>%
-#   ggplot(data = ., aes(x = epoch, y = error_mean, color = lang)) +
-#   geom_line()
-# 
-# scores %>% 
-#   filter(experiment == "mixed") %>%
-#   group_by(epoch) %>%
-#   summarise(error_mean = mean(score)) %>%
-#   ggplot(data = ., aes(x = epoch, y = error_mean)) +
-#   geom_line()
-# 
-# scores %>% 
-#   filter(experiment == "sjd") %>%
-#   group_by(epoch) %>%
-#   summarise(error_mean = mean(score)) %>%
-#   arrange(error_mean)
-# 
-# scores %>% 
-#   filter(experiment == "sjd") %>%
-#   group_by(epoch) %>%
-#   summarise(error_mean = mean(score)) %>%
-#   ggplot(data = ., aes(x = epoch, y = error_mean)) +
-#   geom_line()
-
 run_model <- function(model = "models/mixed/mixed-00020000.pyrnn.gz", folder = "test/*/*.bin.png"){
   system(str_glue("/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rpred -q -Q 4 -m {model} '{folder}'"))
 }
@@ -54,6 +16,7 @@ evaluate_model <- function(model = "mixed", test_set = "test/*/*.bin.png"){
     glue::glue_collapse(sep = " ") %>%
     map_df(~ {tibble(model = model,
            errors = str_extract(.x, "(?<=errors )\\d+"),
+           characters = str_extract(.x, "(?<=total )\\d+"),
            percent = str_extract(.x, "(?<=err )[\\d.]+"),
            test = test_set)})
   result
@@ -73,6 +36,10 @@ score_sjd <- tests %>%
 
 scores <- bind_rows(score_mixed, score_sjd)
 
+scores %>% mutate(test = str_extract(test, "(mns|sjd|yrk|sel)")) %>%
+  select(model, test, errors, characters, percent) %>%
+  knitr::kable(format = "latex")
+
 score_plot <- scores %>% mutate(model_type = str_extract(model, "(sjd|mixed)")) %>%
   mutate(test = str_extract(test, "(mns|sjd|yrk|sel)")) %>%
   mutate(error_rate = as.double(percent)) %>%
@@ -84,34 +51,4 @@ score_plot <- scores %>% mutate(model_type = str_extract(model, "(sjd|mixed)")) 
        y = "Error rate (%)")
 
 ggsave(filename = "plots/figure_5.png", plot = score_plot)
-
-dir("test", pattern = "prob$", full.names = TRUE, recursive = TRUE) %>%
-  map_df(~ {
-    read_tsv(.x, col_names = c("char", "prob")) %>%
-        mutate(lang = str_extract(.x, "(mns|sel|sjd|yrk)"))
-               }
-    ) %>%
-  group_by(char) %>%
-  summarise(error_mean = mean(prob)) %>%
-  arrange(error_mean)
-
-
-gt %>% group_by(page, lang, orig_page) %>%
-  mutate(chars = nchar(line)) %>%
-  summarise(page_chars = sum(chars)) %>%
-  arrange(page_chars) %>% View
-
-training_corpus <- dir("train", pattern = "gt.txt$", full.names = TRUE) %>%
-  map_df(~ {tibble(line = read_file(.x),
-                   file = .x)})
-
-training_corpus %>%
-  unnest_tokens(output = "chars", 
-                input = line, 
-                token = "regex", 
-                pattern = "|", 
-                to_lower = FALSE) %>%
-  mutate(lang = str_extract(file, "(mns|sel|sjd|yrk)")) %>%
-  group_by(lang) %>%
-  count(chars, sort = TRUE)  %>% View
 
