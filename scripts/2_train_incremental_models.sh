8cad1c4f027b1a16caaa61bfaafb3e32a8707694
Niko Partanen <nikotapiopartanen@gmail.com>
2019-01-04 17:06:59 +0200

    harmonizing the scripts…
    … so that they may work on someone elses computer too

--- scripts/2_train_incremental_models.sh
+++ scripts/2_train_incremental_models.sh
@@ -3,5 +3,5 @@
 for filelist in `ls training_lists/*.txt`
 do
  number=$(echo $filelist | sed 's/training_lists\/sjd-train-//g' | sed 's/.txt//g')
- /Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain --file $filelist -F 2000 -c ./train/sjd/*gt.txt --ntrain 10000 -o models/incr/sjd-$number
+ ocropus-rtrain --file $filelist -F 2000 -c ./train/sjd/*gt.txt --ntrain 10000 -o models/incr/sjd-$number
 done
