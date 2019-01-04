24245fdc92c458da92b00a70190d86e6ffa5f146
Niko Partanen <nikotapiopartanen@gmail.com>
2019-01-04 17:09:12 +0200

    harmonizing the scripts…
    … so that they may work on someone elses computer too

--- scripts/3_evaluate_models.sh
+++ scripts/3_evaluate_models.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for model in `ls models/mixed/*00020000*`
+for model in `ls models/mixed/*00050000*`
 do
  model_score=`echo ${model##*/}`
  ocropus-rpred --probabilities -q -Q 4 -m $model 'test/*/*.bin.png'
  ocropus-econf -e evaluation/mixed/${model_score}_score.txt test/*/*gt.txt
 done
 
-for model in `ls models/sjd/*00020000*`
+for model in `ls models/sjd/*00050000*`
 do
  model_score=`echo ${model##*/}`
  ocropus-rpred --probabilities -q -Q 4 -m $model 'test/*/*.bin.png'
