2662ca31aa7fb35c1c4d565b0eeddd7898baccbe
Niko Partanen <nikotapiopartanen@gmail.com>
2019-01-04 17:11:01 +0200

    harmonizing the scripts…
    … so that they could work on someone elses computer too

--- scripts/4_evaluate_page_increase.sh
+++ scripts/4_evaluate_page_increase.sh
@@ -3,9 +3,11 @@
 mkdir evaluation
 mkdir evaluation/incr
 
+# the test is done here for each model trained for 10,000 steps
+
 for model in `ls models/incr/*10000.pyrnn.gz`
 do
   model_score=`echo ${model##*/}`
-  /Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rpred -q -Q 4 -m $model 'test/sjd/*.bin.png'
+  ocropus-rpred -q -Q 4 -m $model 'test/sjd/*.bin.png'
   ocropus-econf -e evaluation/incr/${model_score}_score.txt test/sjd/*gt.txt
 done
