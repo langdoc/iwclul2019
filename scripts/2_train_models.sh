99cf78862f6eceb910471bf3bbe973d318878d6c
Niko Partanen <nikotapiopartanen@gmail.com>
2019-01-04 17:07:28 +0200

    harmonizing the scripts…
    … so that they may work on someone elses computer too

--- scripts/2_train_models.sh
+++ scripts/2_train_models.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain -F 2500 -c ./train/mixed/*gt.txt --ntrain 50000 -o models/mixed/mixed ./train/mixed/*bin.png
-/Users/niko/anaconda3/envs/ocropus_env/bin/ocropus-rtrain -F 2500 -c ./train/sjd/*gt.txt --ntrain 50000 -o models/sjd/sjd ./train/sjd/*bin.png
+ocropus-rtrain -F 2500 -c ./train/mixed/*gt.txt --ntrain 50000 -o models/mixed/mixed ./train/mixed/*bin.png
+ocropus-rtrain -F 2500 -c ./train/sjd/*gt.txt --ntrain 50000 -o models/sjd/sjd ./train/sjd/*bin.png
